4d9a7d13499eddfc2a4409c3088c5c45de298ceb
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-16 14:40:22 +0330

    feat: add model user

--- shahkar_sample/shahkar_user/models.py
+++ shahkar_sample/shahkar_user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+
+class User(models.Model):
+    phonenumber = models.CharField()
+    nathinal_id = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birthday = models.DateTimeField()
+    address = models.CharField(max_length=255)
+
+    def clean_phonenumber(self):
+        if len(self.phonenumber) != 11:
+            raise ValidationError(message="phone number must be 11 integer")
