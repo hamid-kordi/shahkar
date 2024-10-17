07272e78b7383fb73e11664fa8ed0dbb99da45ee
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-17 14:39:36 +0330

    add django setup

--- shahkar_sample/shahkar_sample/celery.py
+++ shahkar_sample/shahkar_sample/celery.py
@@ -1,7 +1,9 @@
 import os
 from celery import Celery
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shahkar_sample.settings")
+django.setup()
 app = Celery("shahkar_sample")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
