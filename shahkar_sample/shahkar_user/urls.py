23094e211230d08ee93eded83530622bf1e0c1ff
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-17 10:10:05 +0330

    add url

--- shahkar_sample/shahkar_user/urls.py
+++ shahkar_sample/shahkar_user/urls.py
@@ -1,8 +1,11 @@
-
-from django import urls
-
-
+from django.urls import path
+from .views import ViewUserData
 
 app_name = "user"
 
 urlpatterns = []
+
+
+urlpatterns = [
+    path("my_view/", ViewUserData.as_view(), name="my_view"),
+]
