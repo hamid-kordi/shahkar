04f226af931f57b4b38bf657d312fa17f99bf113
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-17 10:21:16 +0330

    feat:config celery

---
+++ shahkar_sample/shahkar_sample/celery.py
@@ -0,0 +1,6 @@
+import os
+from celery import Celery
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shahkar_sample.settings')
+app = Celery('shahkar_sample')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
