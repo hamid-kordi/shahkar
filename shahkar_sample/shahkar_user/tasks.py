0e4232b3f40a5d5bd8b5d9b154ae2ae08d57ffa0
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-17 11:15:18 +0330

    add cahce to spark

--- shahkar_sample/shahkar_user/tasks.py
+++ shahkar_sample/shahkar_user/tasks.py
@@ -10,9 +10,11 @@ def find_user_by_phone(phone_number):
     jdbc_url = "jdbc:postgresql://shahkar_postgres:5432/shahkar"
     properties = {"user": "username", "password": "1234"}
     df = spark.read.jdbc(url=jdbc_url, table="user", properties=properties)
+    df = df.repartition(100)
+    df.cache()
     user = df.filter(df.phonenumber == phone_number).collect()
     if user:
-        user = user[0]  # فرض می‌کنیم تنها یک کاربر با این شماره تلفن وجود دارد
+        user = user[0]
         return {
             "national_id": user.natoinal_id,
             "first_name": user.first_name,
