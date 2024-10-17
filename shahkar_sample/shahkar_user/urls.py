c95d061e8017fdd967b1d946154a6927f0d4ad0a
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-17 10:47:02 +0330

    add result view url

--- shahkar_sample/shahkar_user/urls.py
+++ shahkar_sample/shahkar_user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ViewUserData
+from .views import ViewUserData, GetTaskResult
 
 app_name = "user"
 
@@ -8,4 +8,5 @@ urlpatterns = []
 
 urlpatterns = [
     path("my_view/", ViewUserData.as_view(), name="my_view"),
+    path("task_result/", GetTaskResult.as_view(), name="result"),
 ]
