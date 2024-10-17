bd25751ed2396a8c1527aa75f19a726a900eb498
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-17 10:27:51 +0330

    add celery config

--- shahkar_sample/shahkar_sample/settings.py
+++ shahkar_sample/shahkar_sample/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "shahkar_user",
     "drf_spectacular",
+    'django_celery_results',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Tehran'
+CELERY_TRACK_STARTED = True
