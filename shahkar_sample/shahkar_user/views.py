3f97e2a6691f6c0e01d289212367a44b6047c971
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-17 10:49:59 +0330

    add function view

--- shahkar_sample/shahkar_user/views.py
+++ shahkar_sample/shahkar_user/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.views import APIView 
-# Create your views here.
+from rest_framework.views import APIView
+from drf_spectacular.utils import extend_schema
+from .serializers import RequestSerializer, ResponseSerializer
+from .tasks import find_user_by_phone
+from celery.result import AsyncResult
 
 
 class ViewUserData(APIView):
-    pass 
+    @extend_schema(
+        request=RequestSerializer,
+        responses={202, ResponseSerializer},
+        description="Get user data",
+    )
+    def get(self, request):
+        analyzer_id = request.GET.get("analyzer_id")
+        if not phone_number:
+            return Response({"error": "Phone number is required."}, status=400)
+        phone_number = request.GET.get("phone_number")
+        task = find_user_by_phone.delay(phone_number)
+        return Response({"task_id": task.id}, status=202)
+
+
+class GetTaskResult(APIView):
+    def get(self, reqest, task_id):
+        result = AsyncResult(id=task_id)
+        if result.state == "PENDING":
+            return Response({"status": "Pending"}, status=202)
+        if result.state == "SUCCESS":
+            return Response(result.result, status=200)
+        if result.state == "STARTED":
+            return Response({"status": "Started"}, status=202)
+        else:
+            return Response(
+                {"status": result.state, "message": str(result.info)}, status=400
+            )
