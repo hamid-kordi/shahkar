6b09955cdfa83a87faa955f42367ce7ca879fb51
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-20 19:10:51 +0330

    edit celery config

--- shahkar_sample/shahkar_sample/celery.py
+++ shahkar_sample/shahkar_sample/celery.py
@@ -12,9 +12,9 @@ app.autodiscover_tasks()
 app.conf.broker_url = settings.CELERY_BROKER_URL
 app.conf.broker_connection_retry_on_startup = True
 app.conf.imports = ["shahkar_user.tasks"]
-app.conf.result_backend = settings.CELERY_BROKER_URL
+app.conf.result_backend = settings.CELERY_RESULT_BACKEND
 app.conf.task_serializer = settings.CELERY_TASK_SERIALIZER
-app.conf.result_serializer = settings.CELERY_RESULT_BACKEND
+app.conf.result_serializer = settings.CELERY_RESULT_SERIALIZER
 app.conf.accept_content = ["json"]
 app.conf.result_expires = timedelta(days=1)
 app.conf.task_always_eager = False
