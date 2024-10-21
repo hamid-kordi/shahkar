d7ebe7c22d5776aa74238a81c474d949ca2ee1d4
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-21 17:07:12 +0330

    edit necassery command

--- shahkar_sample/entrypoint.sh
+++ shahkar_sample/entrypoint.sh
@@ -3,5 +3,3 @@
 # Start the Gunicorn server in the background
 gunicorn --bind 0.0.0.0:8000 myproject.wsgi:application &
 
-# locust -f test_get_data.py --worker --master-host=127.0.0.1 --logfile locust.log
-#locust -f test_get_data.py --master --web-port 8081 --logfile locust.log
