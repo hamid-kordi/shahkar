abc571b8d50daa5c70c7577fd2b579a4cc9433e4
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-22 08:32:11 +0330

    ir time zone

--- shahkar_sample/shahkar_sample/settings.py
+++ shahkar_sample/shahkar_sample/settings.py
@@ -127,12 +127,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
+TIME_ZONE = 'Asia/Tehran'
 USE_TZ = True
 
+USE_I18N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
