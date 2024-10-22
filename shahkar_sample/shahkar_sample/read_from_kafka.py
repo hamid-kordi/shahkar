8a8ff82740086873dc5eba5fcb288b2522fe506e
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-22 21:03:42 +0330

    add send get kafka

--- shahkar_sample/shahkar_sample/read_from_kafka.py
+++ shahkar_sample/shahkar_sample/read_from_kafka.py
@@ -1,20 +1,44 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, from_json, count
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    FloatType,
+    IntegerType,
+    TimestampType,
+)
 
-spark = SparkSession.builder \
-    .appName("Kafka to Spark") \
-    .getOrCreate()
+spark = SparkSession.builder.appName("Kafka to Spark").getOrCreate()
 
-kafka_df = spark.readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "kafka:9092") \
-    .option("subscribe", "api_log") \
+schema = StructType(
+    [
+        StructField("timestamp", TimestampType(), nullable=True),
+        StructField("task_id", StringType(), nullable=True),
+        StructField("analyzer_id", StringType(), nullable=True),
+        StructField("phonenumber", StringType(), nullable=True),
+        StructField("response_time", FloatType(), nullable=True),
+        StructField("result_time", TimestampType(), nullable=True),
+        StructField("status", StringType(), nullable=True),
+        StructField("request_size", IntegerType(), nullable=True),
+    ]
+)
+
+kafka_df = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", "kafka:9092")
+    .option("subscribe", "api_log")
     .load()
+)
 
-processed_logs = kafka_df.selectExpr("CAST(value AS STRING)")
 
-query = processed_logs.writeStream \
-    .outputMode("append") \
-    .format("console") \
-    .start()
+api_log_df = kafka_df.select(
+    from_json(col("value").cast("string"), schema).alias("data")
+).select("data.*")
 
+aggregated_df = api_log_df.groupBy("analyzer_id").agg(
+    count("*").alias("total_requests")
+)
+
+query = aggregated_df.writeStream.outputMode("complete").format("console").start()
 query.awaitTermination()
--- shahkar_sample/shahkar_sample/send_to_kafka.py
+++ shahkar_sample/shahkar_sample/send_to_kafka.py
@@ -1,18 +1,49 @@
 from kafka import KafkaProducer
 import psycopg2
 import json
+from datetime import datetime
+import time
+
+def json_serial(obj):
+    """Function to handle non-serializable types."""
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    raise
+
 
 conn = psycopg2.connect(
-    dbname="postgres", user="username", password="1234", host="postgres", port="5433"
+    dbname="postgres", user="username", password="1234", host="postgres", port="5432"
 )
 
-producer = KafkaProducer(bootstrap_servers="localhost:9092")
+producer = KafkaProducer(bootstrap_servers="kafka:9092")
 
-with conn.cursor() as cur:
-    cur.execute("SELECT * FROM shahkar_user_apilog")
-    rows = cur.fetchall()
-    for row in rows:
-        producer.send("api_log", json.dumps(row).encode("utf-8"))
+coutn = 0
+while True:
+    try:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM shahkar_user_apilog")
+            rows = cur.fetchall()
+            for row in rows:
+                producer.send("api_log", json.dumps(row, default=json_serial).encode("utf-8"))
+                coutn += 1
+            print(f"{coutn} rows")
+            producer.flush()
+        time.sleep(60)
+    except:
+        conn.close()
+        print("connection closed")
 
-producer.flush()
-conn.close()
+# from kafka import KafkaConsumer
+# import json
+
+# consumer = KafkaConsumer(
+#     'api_log',
+#     bootstrap_servers='kafka:9092',
+#     auto_offset_reset='earliest',
+#     enable_auto_commit=True,
+#     group_id='my-group'
+# )
+
+# for message in consumer:
+#     data = json.loads(message.value.decode('utf-8'))
+#     print(data)  
